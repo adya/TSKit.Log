127428168fff5c83ee37b442019674aa2e0a92ae
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2020-04-09 23:01:19 +0300

    Updated copyrights.

--- TSKit.Log/Sources/AnyLogEntryDecorator.swift
+++ TSKit.Log/Sources/AnyLogEntryDecorator.swift
@@ -1,6 +1,6 @@
 // - Since: 01/20/2018
 // - Author: Arkadii Hlushchevskyi
-// - Copyright: © 2019. Arkadii Hlushchevskyi.
+// - Copyright: © 2020. Arkadii Hlushchevskyi.
 // - Seealso: https://github.com/adya/TSKit.Log/blob/master/LICENSE.md
 
 /// Decorates log entry before its being sent to output.
